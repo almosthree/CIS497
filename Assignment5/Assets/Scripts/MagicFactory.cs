b7a2aadfc3aa2c3658b0be58a3b2c739a61095ee
Jerod Lockhart <j.r.1327@gmail.com>
2021-03-01 18:04:06 -0600

    1

--- Assignment5/Assets/Scripts/MagicFactory.cs
+++ Assignment5/Assets/Scripts/MagicFactory.cs
@@ -2,17 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagicFactory : MonoBehaviour
+namespace SimpleFactoryPatternWithGameObjects
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    //Attach this class to an empty GameObject
+    public class MagicFactory : MonoBehaviour
     {
-        
+        public GameObject IcePrefab;
+        public GameObject FirePrefab;
+        public GameObject ThunderPrefab;
+
+        private GameObject enemyToSpawn;
+
+        public GameObject CreateEnemy(string type)
+        {
+            enemyToSpawn = null;
+
+            if (type.Equals("Zombie"))
+            {
+                enemyToSpawn = IcePrefab;
+
+                //If there is not already a Zombie script attached to the prefab, attach one
+                if (enemyToSpawn.GetComponent<Zombie>() == null)
+                {
+                    enemyToSpawn.AddComponent<Zombie>();
+                }
+            }
+            else if (type.Equals("Vampire"))
+            {
+                enemyToSpawn = FirePrefab;
+
+                //If there is not already a Vampire script attached to the prefab, attach one
+                if (enemyToSpawn.GetComponent<Vampire>() == null)
+                {
+                    enemyToSpawn.AddComponent<Vampire>();
+                }
+
+            }
+            else if (type.Equals("Werewolf"))
+            {
+                enemyToSpawn = ThunderPrefab;
+
+                //If there is not already a Werewolf script attached to the prefab, attach one
+                if (enemyToSpawn.GetComponent<Werewolf>() == null)
+                {
+                    enemyToSpawn.AddComponent<Werewolf>();
+                }
+
+            }
+
+            Debug.Log("Factory sending: " + enemyToSpawn);
+            return enemyToSpawn;
+        }
+
     }
 }
