5686fbf0b7b617d3f239f43eacef462a2c8be40c
Jerod Lockhart <j.r.1327@gmail.com>
2021-03-07 23:54:14 -0600

    Update Unit.cs

--- Assignment2/Assets/Scripts/Unit.cs
+++ Assignment2/Assets/Scripts/Unit.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿/* * (Jerod Lockhart) 
+ * * (Unit) 
+ * * (Assignemnt2) 
+ * * (This is the Unit class) 
+ */
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Unit
 {
     float speed;
+
 
 }
