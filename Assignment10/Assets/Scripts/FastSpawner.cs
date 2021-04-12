a073468b3cff294167a74e2ae5df619e6ba5dc08
Jerod Lockhart <j.r.1327@gmail.com>
2021-04-11 23:55:07 -0500

    A10

---
+++ Assignment10/Assets/Scripts/FastSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A10
+{
+    public class FastSpawner : MonoBehaviour
+    {
+
+        //We could call our ObjectPooler Singleton like this
+        /*
+        void FixedUpdate()
+        {
+            ObjectPooler.instance.SpawnFromPool("Sphere", transform.position, Quaternion.identity);
+        }
+        */
+
+        //But creating a reference to our ObjectPooler may be faster in terms of performance
+        ObjectPooler objectPooler;
+
+        private void Start()
+        {
+            objectPooler = ObjectPooler.instance;
+        }
+
+        void FixedUpdate()
+        {
+            objectPooler.SpawnFromPool("Coin", transform.position, Quaternion.identity);
+        }
+    }
+}
