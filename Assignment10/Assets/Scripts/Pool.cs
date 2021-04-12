6a7db4cb946a4c1c50d19543b861599f88f1ed1b
Jerod Lockhart <j.r.1327@gmail.com>
2021-04-11 23:39:51 -0500

    A10

---
+++ Assignment10/Assets/Scripts/Pool.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace A10 {
+
+    [System.Serializable]
+ 
+    public class Pool
+    {
+
+        //All of these need to be set in the inspector for each pool
+        public string tag;
+        public GameObject prefab;
+        public int size;
+
+}
+}
---
+++ Assignment10/Assets/Scripts/Spawner.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace A10 { 
+    public class Spawner : MonoBehaviour
+    {
+
+        public GameObject prefabToSpawn;
+
+        void FixedUpdate()
+        {
+            Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+        }
+    }
+}
